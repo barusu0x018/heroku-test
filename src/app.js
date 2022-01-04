7f13bc80752868e0ced10b8e7e482ca1b208094f
subaru_takasu <subaru_takasu@bita.jp>
2022-01-04 17:59:39 +0900

    fix: modifying not open app.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 8080;
 
-const server = app.listen(8080, function(){
+const server = app.listen(port, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
