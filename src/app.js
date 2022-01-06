7e2c70d021b200018303549a47d6f1a81c35cc0b
subaru_takasu <subaru_takasu@bita.jp>
2022-01-06 11:15:28 +0900

    fix: create link menu.

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const server = app.listen(port, function(){
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.use('statics', express.static(__dirname + '/statics'));
+app.use('test', express.static(__dirname + '/test'));
 app.get('/', (req, res) => {
     res.render('index')
 });
